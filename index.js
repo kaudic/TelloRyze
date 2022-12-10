13ae77d2b67c03da126984599844a806e0a79aa1
Killian Audic <killianaudic@aol.fr>
2022-12-10 17:30:00 +0100

    put my code in order for people to make it work at home

--- index.js
+++ index.js
@@ -1,80 +1,79 @@
 // imports modules
-const https = require('https');
+const http = require('http');
 require('dotenv').config();
 const WebSocket = require('ws');
 const spawn = require('child_process').spawn;
 const fs = require('fs');
-
 
 // create a server to render the web page (controllers for drone, video player and indicators for Drone State)
 const app = require('./app');
 const port = process.env.PORT ?? 3000;
 
 // Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.planitools.com/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/www.planitools.com/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/www.planitools.com/chain.pem', 'utf8');
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.planitools.com/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/www.planitools.com/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/www.planitools.com/chain.pem', 'utf8');
 
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca
-};
+// const credentials = {
+//   key: privateKey,
+//   cert: certificate,
+//   ca: ca
+// };
 
-const server = https.createServer(credentials, app);
+const server = http.createServer(app);
 
 // application à l'écoute d'un port
 server.listen(port, () => console.log(`Navigator listenning on port ${port}`));
 
-// // initialize a websocket for pushing state from Drone
-// // socket for the Drone state
-// const WebSocketIo = require('./app/class/webSocketIo');
-// const websocket = new WebSocketIo(server);
+// initialize a websocket for pushing state from Drone
+// socket for the Drone state
+const WebSocketIo = require('./app/class/webSocketIo');
+const websocket = new WebSocketIo(server);
 
-// //------------------------------------------------------------------------------------------------------------------------------
-// // create a second server to stream the video from Drone camera and forward to another webSocket
-// const streamPort = process.env.STREAM_SERVER_PORT ?? 3001;
-// const streamServer = http.createServer((req, res) => {
+//------------------------------------------------------------------------------------------------------------------------------
+// create a second server to stream the video from Drone camera and forward to another webSocket
+const streamPort = process.env.STREAM_SERVER_PORT ?? 3001;
+const streamServer = http.createServer((req, res) => {
 
-//     // Log that a stream connection has come through
-//     console.log(`Stream Connection on from: ${req.socket.remoteAddress}:${req.socket.remotePort}`);
+  // Log that a stream connection has come through
+  console.log(`Stream Connection on from: ${req.socket.remoteAddress}:${req.socket.remotePort}`);
 
-//     // listenning to data event on the stream server - ffmpeg will send data through the spawn child process
-//     req.on('data', (data) => {
-//         webSocketStreamServer.clients.forEach(function each(client) {
-//             if (client.readyState === WebSocket.OPEN) {
-//                 client.send(data);
-//             }
-//         });
-//     });
+  // listenning to data event on the stream server - ffmpeg will send data through the spawn child process
+  req.on('data', (data) => {
+    webSocketStreamServer.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
 
-// }).listen(streamPort, () => console.log(`Stream port is on and listenning on ${streamPort}`));
+}).listen(streamPort, () => console.log(`Stream port is on and listenning on ${streamPort}`));
 
-// // initialize a websocket for pushing video stream from Drone to browser
-// const webSocketStreamServer = new WebSocket.Server({
-//     server: streamServer
-// });
+// initialize a websocket for pushing video stream from Drone to browser
+const webSocketStreamServer = new WebSocket.Server({
+  server: streamServer
+});
 
-// // launching the encoding and piping from the Drone, after a delay of 3seconds
-// setTimeout(function () {
-//     var args = [
-//         "-i", "udp://0.0.0.0:11111",
-//         "-r", "30",
-//         "-s", "960x720",
-//         "-codec:v", "mpeg1video",
-//         "-b", "800k",
-//         "-f", "mpegts",
-//         `http://127.0.0.1:${process.env.STREAM_SERVER_PORT}/stream`
-//     ];
+// launching the encoding and piping from the Drone, after a delay of 3seconds
+setTimeout(function () {
+  var args = [
+    "-i", "udp://0.0.0.0:11111",
+    "-r", "30",
+    "-s", "960x720",
+    "-codec:v", "mpeg1video",
+    "-b", "800k",
+    "-f", "mpegts",
+    `http://127.0.0.1:${process.env.STREAM_SERVER_PORT}/stream`
+  ];
 
-//     // Spawn an ffmpeg instance
-//     var streamer = spawn('ffmpeg', args);
-//     // Uncomment if you want to see ffmpeg stream info
-//     //streamer.stderr.pipe(process.stderr);
-//     streamer.on("exit", function (errCode) {
-//         console.log("Failure with code : " + errCode);
-//     });
-// }, 3000);
+  // Spawn an ffmpeg instance
+  var streamer = spawn('ffmpeg', args);
+  // Uncomment if you want to see ffmpeg stream info
+  //streamer.stderr.pipe(process.stderr);
+  streamer.on("exit", function (errCode) {
+    console.log("Failure with code : " + errCode);
+  });
+}, 3000);
 
 
 
